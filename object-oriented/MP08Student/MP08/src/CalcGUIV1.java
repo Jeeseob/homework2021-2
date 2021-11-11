91e311019c076f3e3887566328ae282507b07919
Jeeseob <jeeseob5761@gmail.com>
2021-11-11 11:37:37 +0900

    프로그래밍 언어론&고급객체지향

--- object-oriented/MP08Student/MP08/src/CalcGUIV1.java
+++ object-oriented/MP08Student/MP08/src/CalcGUIV1.java
@@ -16,13 +16,16 @@ public class CalcGUIV1 extends JFrame implements ActionListener {
     Dimension buttonDimension = new Dimension(BUTTON_WIDTH, COMPONENT_HEIGHT);
 
     JLabel display = new JLabel(); // 숫자 값 및 결과 출력 화면
+    NumberButtonCommand numberButtonCommand;
+    EqualButtonCommand equalButtonCommand;
+    OperatorButtonCommand operatorButtonCommand;
+    NoCommand noCommand = new NoCommand();
 
     Invoker invoker = new Invoker();
 
     CalcGUIV1() {
         super("CalcGUIV1");
         calculator = new Calculator();
-
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         Font labelFont = display.getFont();
         display.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -63,9 +66,58 @@ public class CalcGUIV1 extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton cmdButton = (JButton) e.getSource();
-        invoker.setCommand(new ConcreteCommandButton(buttons));
+        numberButtonCommand = new NumberButtonCommand(calculator,cmdButton, display);
+        equalButtonCommand = new EqualButtonCommand(calculator, display);
+        operatorButtonCommand = new OperatorButtonCommand(calculator, cmdButton);
+        invoker.setCommand(noCommand);
+
+        if (cmdButton == buttons[0] || cmdButton == buttons[1]  || cmdButton == buttons[2]  ||
+                cmdButton == buttons[3]  || cmdButton == buttons[4]  || cmdButton == buttons[5]  ||
+                cmdButton == buttons[6]  || cmdButton == buttons[7]  || cmdButton == buttons[8]  ||
+                cmdButton == buttons[9] ) { // 0-9 버튼
+            invoker.setCommand(numberButtonCommand);
+//            if (calculator.isOperand1Set() && calculator.isOperatorSet()) { // 첫 번째 피연산자와 연산자가 지정되었다면 두 번째 피연산자 값으로 사용
+//                int num2 = Integer.parseInt(cmdButton.getText());
+//                calculator.setOperand2(num2);
+//                display.setText("" + num2);
+//                calculator.setOperand2Set(true);
+//            }
+//            else {  // 첫 번째 피연산자 값 지정
+//                int num1 = Integer.parseInt(cmdButton.getText());
+//                display.setText("" + num1);
+//                calculator.setOperand1(num1);
+//                calculator.setOperand1Set(true);
+//            }
+        }
+        else if (cmdButton == buttons[14]) { // = 버튼
+            invoker.setCommand(equalButtonCommand);
+//            int result = 0;
+//            if (calculator.isOperand1Set() && calculator.isOperand2Set() && calculator.isOperatorSet()) { // 두 개 피 연산자값과 연산자가 지정되었다면
+//                if (calculator.getOperator() == '+') {
+//                    result = calculator.getOperand1() + calculator.getOperand2();
+//                }
+//                else if (calculator.getOperator() == '-') {
+//                    result = calculator.getOperand1() - calculator.getOperand2();
+//                }
+//                else if (calculator.getOperator() == '*') {
+//                    result = calculator.getOperand1() * calculator.getOperand2();
+//                }
+//                else if (calculator.getOperator() == '/') {
+//                    result = calculator.getOperand1() / calculator.getOperand2();
+//                }
+//            }
+//            display.setText("" + result);
+//            calculator.clearFlags();
+        }
+        else if (cmdButton == buttons[10] || cmdButton == buttons[11] ||
+                cmdButton == buttons[12] || cmdButton == buttons[13]) { // +, -, *, / 버튼
+            invoker.setCommand(operatorButtonCommand);
+//            if (calculator.isOperand1Set()) {  // 첫 번째 피연산자 값이 지정되어야만 연산자 처리 가능
+//                calculator.setOperatorSet(true);
+//                calculator.setOperator(cmdButton.getText().charAt(0));
+//            }
+        }
         invoker.pressButton();
-        display.setText("" + result);
     }
 
     public static void main(String[] args) {
--- object-oriented/MP08Student/MP08/src/Command.java
+++ object-oriented/MP08Student/MP08/src/Command.java
@@ -1,3 +1,3 @@
 public interface Command {
-    String execute();
+    void execute();
 }
